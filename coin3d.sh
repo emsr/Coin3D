620c5e69562984016bc344c34fe51f0aa3e40b0c
Ed Smith-Rowland <3dw4rd@verizon.net>
2018-10-16 15:14:30 -0400

    Add another useful Qt5 for Help.

--- coin3d.sh
+++ coin3d.sh
@@ -11,6 +11,7 @@ function clone_and_build {
 }
 
 apt update
+apt install -y apt-tools
 
 apt install -y graphviz
 apt install -y doxygen
@@ -18,10 +19,12 @@ apt install -y doxygen
 # Need hg
 apt install -y mercurial
 
-# Need Qt5: Core Gui OpenGL Widgets
+# Need Qt5: Core, Gui, OpenGL, Widgets, ...
 apt install -y qt5-default
 apt install -y qt3d5-dev
 apt install -y libqt5sql5-psql libqt5sql5-sqlite
+# ... and QtHelp...
+apt install -y qttools5-dev
 
 # Now build and install the Coin3D stack
 clone_and_build hg simage
