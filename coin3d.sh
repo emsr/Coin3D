a510bdfb3df63d92ba0c7bf9974726c425369c51
Ed Smith-Rowland <3dw4rd@verizon.net>
2019-03-25 13:13:49 -0400

    Better script.

--- coin3d.sh
+++ coin3d.sh
@@ -1,10 +1,26 @@
 #! /bin/bash
 
-function clone_and_build {
-  $1 clone https://bitbucket.org/Coin3D/$2
-  mkdir $2_build
-  cd $2_build
-  cmake ../$2 -G"Unix Makefiles"
+function clone_and_build
+{
+  if [ -d $1 ]
+  then
+    cd $1
+    hg pull
+    hg update
+    cd ..
+  else
+    hg clone https://bitbucket.org/Coin3D/$1
+  fi
+
+  if [ -d $1_build ]
+  then
+    cd $1_build
+  else
+    mkdir $1_build
+    cd $1_build
+    cmake ../$1 -G"Unix Makefiles"
+  fi
+
   make
   make install
   cd ..
@@ -22,11 +38,15 @@ apt install -y mercurial
 # Need Qt5: Core, Gui, OpenGL, Widgets, ...
 apt install -y qt5-default
 apt install -y qt3d5-dev
+apt install -y libqt53dcore5 libqt53drender5 libqt53dlogic5 libqt53dinput5 libqt53dextras5
 apt install -y libqt5sql5-psql libqt5sql5-sqlite
 # ... and QtHelp...
 apt install -y qttools5-dev
 
+# Boost
+apt install -y libboost-all-dev
+
 # Now build and install the Coin3D stack
-clone_and_build hg simage
-clone_and_build hg coin
-clone_and_build hg soqt
+clone_and_build simage
+clone_and_build coin
+clone_and_build soqt
